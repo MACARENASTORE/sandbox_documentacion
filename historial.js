7ec448472c82773b3b55912ca80590298e418093
MACARENASTORE <oclusiva9204@gmail.com>
2023-04-21 07:45:52 -0500

    committed 2

--- historial.js
+++ historial.js
@@ -1,3 +1,4 @@
 //npm install
 //npm install express typescript
 //npm install --save-dev @types/node @types/express
+//npx ts-node src/index.ts
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import App from "./Router"
+
+//Crear un objeto de la clase App
+
+const app=new App()
+
+app.start()
